8a435954f1e3877f461395ce49305a9a1f816f88
dkzks44 <dkzks44@users.noreply.github.com>
2023-05-10 17:43:02 +0900

    complete obstacle course

---
+++ unity3D/ObstacleMazeMiniGame/ObstacleMazeMiniGame/Assets/Scripts/Spinner.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    [SerializeField] float xValue = 0;
+    [SerializeField] float yValue = 1f;
+    [SerializeField] float zValue = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        transform.Rotate(xValue,yValue,zValue);
+    }
+}
